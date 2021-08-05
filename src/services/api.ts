c4c5a59b5c5e8cd373287a851250e3f74f5836f7
Vagner Reis <vagnereis10@gmail.com>
2021-08-05 14:30:18 -0300

    test completed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-import Cookie from "js-cookie";
-
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-});
-
-export const source = axios.CancelToken.source();
-
-api.interceptors.request.use(async (config) => {
-  const token = Cookie.get("@");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  
-  return config;
 });
